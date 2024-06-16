c7f957755a748d6ff05e3a3dffe62e17d86493ed
Utsav Yadav <utsavy18@gmail.com>
2024-06-16 15:23:48 +0530

    regiser login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Validator;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register()
+    {
+        $validator = Validator::make(request()->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = new User;
+        $user->name = request()->name;
+        $user->email = request()->email;
+        $user->password = bcrypt(request()->password);
+        $user->save();
+
+        return response()->json($user, 201);
+    }
+
+    /**
+     * Get a JWT via given credentials.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (!$token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        $userCount = User::count();
+
+        return $this->respondWithToken($token, $userCount);
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     * @param  int $userCount
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token, $userCount = null)
+    {
+        $response = [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60,
+        ];
+
+        if ($userCount !== null) {
+            $response['user_count'] = $userCount;
+        }
+
+        return response()->json($response);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\AuthController;
 use  App\Http\Controllers\FeedbackController;
 use  App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
@@ -11,6 +13,18 @@ Route::get('/user', function (Request $request) {
 Route::post('/feedback',[FeedbackController::class,'feedback']);
 Route::get('/feedback/all',[FeedbackController::class,'show']);
 
-Route::resource('products', ProductController::class)->except(['update']);
-Route::post('products/{product}', [ProductController::class, 'update'])->name('products.update');
+Route::get('products', [ProductController::class, 'index'])->name('products.list'); 
+Route::post('products/create', [ProductController::class, 'store'])->name('products.save'); 
+Route::get('products/{product}', [ProductController::class, 'show'])->name('products.view'); 
+Route::delete('products/{product}/delete', [ProductController::class, 'destroy'])->name('products.delete'); 
+Route::post('products/{product}/edit', [ProductController::class, 'update'])->name('products.modify'); 
 
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
+    Route::post('/refresh', [AuthController::class, 'refresh'])->middleware('auth:api')->name('refresh');
+});
